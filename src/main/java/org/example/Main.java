04013940f1c6ceb93f76c5f050b3c65ac5a84383
BuntinMA_Home <BuntinMA@mail.ru>
2022-12-14 20:51:26 +0300

    Задача 2 готова.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,50 @@
-package org.example;public class Main {
+//Реализуйте алгоритм сортировки пузырьком числового массива,
+// результат после каждой итерации запишите в лог-файл.
+
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Привет! ");
+        System.out.printf("Введте размер числового массива: ");
+        String input_text = iScanner.nextLine();
+        int arr_len = Integer.parseInt(input_text);
+        iScanner.close();
+
+        int[] array = new int[arr_len];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+//            System.out.println("Array[i]: " + array[i]);
+        }
+
+        System.out.print("Array: [");
+        for (int i = 0; i < arr_len; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println("]");
+
+        boolean flag = true;
+
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        System.out.print("Sorted array: [");
+        for (int i = 0; i < arr_len; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println("]");
     }
 }
